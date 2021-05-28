accb6e7da030130996a0aa1c05c8cd205adfae5c
TimaVox <zeynalov.teimur@gmail.com>
2021-05-28 15:34:05 +0600

    Minor edits

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -26,6 +26,7 @@ class RolesAndPermissionsSeeder extends Seeder
             'permission_update',
             'legal_view',
             'legal_create',
+            'legal_update',
             'legal_delete',
         ];
 
