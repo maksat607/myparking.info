d455ef6720703960d155ff3306c85e1ab973c00a
TimaVox <zeynalov.teimur@gmail.com>
2021-05-28 16:25:03 +0600

    Added the NPC select 2 plugin

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+/**
+ * Select2.js
+ * */
+
+window.select2 = require('select2');
