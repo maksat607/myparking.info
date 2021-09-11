0f4771b8e51b66cd6857748a4bb69182ef39718a
TimaVox <zeynalov.teimur@gmail.com>
2021-09-11 17:56:55 +0600

    Fix selected curent date

--- resources/js/flatpickr/flatpickr.js
+++ resources/js/flatpickr/flatpickr.js
@@ -5,12 +5,33 @@ if(typeof dateDataViewRequest !== 'undefined' && dateDataViewRequest) {
     dataDefault = dateDataApplication;
 }
 
+function getDate(){
+    if(dataDefault) return dataDefault;
+
+    let today = new Date();
+
+    let curDateDay = today.getDate();
+    let dd = String(curDateDay).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+
+    if(today.getDay() === 0) {
+        dd = String(curDateDay + 1).padStart(2, '0');
+    } else if(today.getDay() === 6) {
+        dd = String(curDateDay + 2).padStart(2, '0');
+    }
+
+    let todayFormat = dd + '/' + mm + '/' + yyyy;
+
+    return todayFormat;
+}
+
 $('.date').flatpickr({
     altInput: true,
     altFormat: "d/m/Y",
     dateFormat: "d-m-Y",
     minDate: (dataDefault) ? dataDefault : "today",
-    defaultDate: (dataDefault) ? dataDefault : "today",
+    defaultDate: getDate(),
     disable: [
         function(date) {
             return (date.getDay() === 0 || date.getDay() === 6);
