b013b05b6796a2c333dc702af4673a3e754ef841
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-05-24 22:06:13 +0600

    photo bug fix

--- resources/views/applications/ajax/modal.blade.php
+++ resources/views/applications/ajax/modal.blade.php
@@ -12,9 +12,8 @@
             </div>
             <div class="car-slide">
 
-              {{ $application->attachments->count() }}
                 @if($application->attachments->isNotEmpty())
-                    @foreach($application->attachments as $attachment)
+                    @foreach($application->attachments->where('file_type','image')->all() as $attachment)
                         <div class="newcart__imgwrap">
                             <a href="{{ $attachment->url }}">
                                 <img src="{{ $attachment->thumbnail_url }}" class="car-row__image">
