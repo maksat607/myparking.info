59781a7b00f4719773e94881b66fd3c5408cfae4
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-09-27 18:14:55 +0600

    Validation error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -56,7 +56,10 @@ class AppServiceProvider extends ServiceProvider
             // Check the table and return true only if there are no entries matching
             // both the first field name and the user input value as well as
             // the second field name and the second field value
-            return DB::table($table)->where($field, $value)->where('status_id','!=', 8)->count() == 0;
+            return DB::table($table)
+                    ->where($field, $value)
+                    ->where($field,'!=' ,'не указан')
+                    ->where('status_id','!=', 8)->count() == 0;
         });
 
 
@@ -69,7 +72,11 @@ class AppServiceProvider extends ServiceProvider
             // Check the table and return true only if there are no entries matching
             // both the first field name and the user input value as well as
             // the second field name and the second field value
-            return DB::table($table)->where($field, $value)->where('status_id','!=', 8)->where('id','!=',$ignore)->count() == 0;
+            return DB::table($table)
+                    ->where($field, $value)
+                    ->where($field,'!=' ,'не указан')
+                    ->where('status_id','!=', 8)
+                    ->where('id','!=',$ignore)->count() == 0;
         });
 
     }
--- app/Traits/NotifyApplicationChanges.php
+++ app/Traits/NotifyApplicationChanges.php
@@ -23,37 +23,37 @@ trait NotifyApplicationChanges
             $item->vin = $item->vin == "не указан" ? null : $item->vin;
             $item->license_plate = $item->license_plate == "не указан" ? null : $item->license_plate;
         });
-        static::updated(function ($item) {
-            Log::info('Updated: ' . collect($item));
-            if ($item->status->id != $item->status_id) {
-                $data = [];
-                if (($item->status_id == Message::$appStatuses['Выдано'] || $item->status_id == Message::$appStatuses['Хранение'] || ($item->issuance)) && !auth()->user()->hasRole('Moderator')) {
-                    ApplicationHasPending::firstOrCreate(['application_id' => $item->id, 'user_id' => auth()->user()->id]);
-                }
-                $data = Message::getApplicationMessage($item, auth()->user());
-                if (count($data) > 0) {
-                    Log::info('Updated: ' . collect($data));
-                    Notification::send(Message::getUsers($item), new UserNotification($data));
-                }
-
-            }
-        });
-        static::deleted(function ($item) {
-            Log::info('Deleted   event call: ' . $item);
-        });
-        static::created(function ($item) {
-
-            $data = [];
-            if ($item->status_id == Message::$appStatuses['Ожидает принятия'] || $item->status_id == Message::$appStatuses['Хранение']) {
-                $data = Message::getApplicationMessage($item, auth()->user());
-
-            }
-            if (count($data) > 0) {
-                Log::info('Created event call: ' . collect($data));
-//                Notification::send(Message::getUsers($item), new UserNotification(($data)));
-            }
-            Log::info('Created event call: ' . $item);
-        });
+//        static::updated(function ($item) {
+//            Log::info('Updated: ' . collect($item));
+//            if ($item->status->id != $item->status_id) {
+//                $data = [];
+//                if (($item->status_id == Message::$appStatuses['Выдано'] || $item->status_id == Message::$appStatuses['Хранение'] || ($item->issuance)) && !auth()->user()->hasRole('Moderator')) {
+//                    ApplicationHasPending::firstOrCreate(['application_id' => $item->id, 'user_id' => auth()->user()->id]);
+//                }
+//                $data = Message::getApplicationMessage($item, auth()->user());
+//                if (count($data) > 0) {
+//                    Log::info('Updated: ' . collect($data));
+//                    Notification::send(Message::getUsers($item), new UserNotification($data));
+//                }
+//
+//            }
+//        });
+//        static::deleted(function ($item) {
+//            Log::info('Deleted   event call: ' . $item);
+//        });
+//        static::created(function ($item) {
+//
+//            $data = [];
+//            if ($item->status_id == Message::$appStatuses['Ожидает принятия'] || $item->status_id == Message::$appStatuses['Хранение']) {
+//                $data = Message::getApplicationMessage($item, auth()->user());
+//
+//            }
+//            if (count($data) > 0) {
+//                Log::info('Created event call: ' . collect($data));
+////                Notification::send(Message::getUsers($item), new UserNotification(($data)));
+//            }
+//            Log::info('Created event call: ' . $item);
+//        });
     }
 //    public static create
 }
