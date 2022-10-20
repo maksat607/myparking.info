c4a80c2c5a13e8b830f40463544c3cd025c2ae69
kuleshovchyngyz <kuleshov.chyngyz@gmail.com>
2022-10-20 15:10:44 +0600

    fix addin photos

--- resources/views/applications/issuance.blade.php
+++ resources/views/applications/issuance.blade.php
@@ -168,7 +168,7 @@
                             </div>
                             Паспорт, доверенность и прочее
                             <div class="page-file-list">
-                                <div class="page-add-file no-ajax">
+                                <div class="page-add-file no-ajax upload-file">
                                     <svg width="40" height="40" viewBox="0 0 40 40" fill="none"
                                          xmlns="http://www.w3.org/2000/svg">
                                         <path opacity="0.6"
